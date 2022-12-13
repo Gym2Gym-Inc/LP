2fe70597baca887c3c81f544bc36d91ef419b9f0
ExeLxrd <tech.mccune@gmail.com>
2022-12-12 23:06:57 -0700

    Updates

---
+++ precache-manifest.c6991f7a2781ed03d44eb750b648738d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92be15a831e55ca967f9a0c9b1dedca",
+    "url": "/Gym2Gym-Inc/LP/index.html"
+  },
+  {
+    "revision": "5f65eaac53979ea23a9e",
+    "url": "/Gym2Gym-Inc/LP/static/css/2.7e814e4b.chunk.css"
+  },
+  {
+    "revision": "fdec806df25d405c45a6",
+    "url": "/Gym2Gym-Inc/LP/static/css/main.ce28bf41.chunk.css"
+  },
+  {
+    "revision": "5f65eaac53979ea23a9e",
+    "url": "/Gym2Gym-Inc/LP/static/js/2.619d7309.chunk.js"
+  },
+  {
+    "revision": "f7ca4bf62dab730414a11c7ee9da82d4",
+    "url": "/Gym2Gym-Inc/LP/static/js/2.619d7309.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdec806df25d405c45a6",
+    "url": "/Gym2Gym-Inc/LP/static/js/main.bd0cc2cf.chunk.js"
+  },
+  {
+    "revision": "d899f535d9e5211d5021",
+    "url": "/Gym2Gym-Inc/LP/static/js/runtime-main.5b890116.js"
+  },
+  {
+    "revision": "8ec6e78e46805e418014c83297b90aee",
+    "url": "/Gym2Gym-Inc/LP/static/media/arrow-right.8ec6e78e.svg"
+  },
+  {
+    "revision": "4889f50931b2a81d662ce83dce6d6a72",
+    "url": "/Gym2Gym-Inc/LP/static/media/done.4889f509.svg"
+  },
+  {
+    "revision": "d4e376e51fc23fe29288f1ef0798393a",
+    "url": "/Gym2Gym-Inc/LP/static/media/mail.d4e376e5.svg"
+  },
+  {
+    "revision": "81a5bb3d71ba46501f57834057c1666d",
+    "url": "/Gym2Gym-Inc/LP/static/media/newsletter.81a5bb3d.svg"
+  },
+  {
+    "revision": "71f30eae279c44ee3362b43ef27f4c04",
+    "url": "/Gym2Gym-Inc/LP/static/media/privacy.71f30eae.md"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Gym2Gym-Inc/LP/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Gym2Gym-Inc/LP/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Gym2Gym-Inc/LP/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Gym2Gym-Inc/LP/static/media/slick.f97e3bbf.svg"
+  }
+]);
